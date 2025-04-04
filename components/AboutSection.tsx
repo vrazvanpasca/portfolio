e3deee42a3d0c54176fce91b3dbdef696cdd8461
vrazvanpasca <v.razvanpasca@gmail.com>
2025-04-04 13:43:13 +0300

    test

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -88,7 +88,7 @@ const AboutSection = () => {
             </div>
             <Image
               src="/superhero.png"
-              alt=""
+              alt="superhero"
               width={325}
               height={325}
               className="hidden md:block md:relative md:bottom-4 md:left-32 md:z-0"
